7e5ebd0a399ef1e771fe533a2a2a8f87840ff1ca
racostyle <cekcar@gmail.com>
2025-04-29 17:32:10 +0200

    Observer updates

--- LibUR/GameStates/GameStateObserver.cs
+++ LibUR/GameStates/GameStateObserver.cs
@@ -1,33 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LibUR.GameStates
 {
     public class GameStateObserver : IGameStateObserver
     {
-        private readonly Dictionary<GameState, Action> _states;
+        private readonly Dictionary<GameState, List<Subscriber>> _states;
 
         public GameStateObserver()
         {
-            _states = new Dictionary<GameState, Action>();
+            _states = new Dictionary<GameState, List<Subscriber>>();
         }
 
-        public void Subscribe(GameState state, Action action)
+        public void Subscribe(GameState state, Action action, string subscriberName)
         {
-            _states.Add(state, action);
+            if (_states.ContainsKey(state))
+            {
+                _states[state].Add(new Subscriber(subscriberName, action));
+                return;
+            }
+
+            if (_states[state].Any(x => x.Name == subscriberName))
+                UnityEngine.Debug.LogWarning($"{subscriberName} already subscribed to {state}");
+            else
+                _states.Add(state, new List<Subscriber> { new Subscriber(subscriberName, action) });
         }
 
-        public void Unsubscribe(GameState state)
+        public void Unsubscribe(GameState state, string subscriberName)
         {
-            _states.Remove(state);
+            if (!_states.ContainsKey(state))
+                return;
+
+            _states[state].RemoveAll(x => x.Name == subscriberName);
         }
 
         public void Fire(GameState state)
         {
             foreach (var st in _states)
             {
-                if (st.Key.Equals(state)) st.Value?.Invoke();
+                if (!st.Key.Equals(state))
+                    continue;
+
+                foreach (var subscriber in st.Value)
+                    subscriber.Action?.Invoke();
             }
+        }
+
+        public void Clear()
+        {
+            _states.Clear();
+        }
+    }
+
+    internal struct Subscriber
+    {
+        internal string Name;
+        internal Action Action;
+
+        public Subscriber(string name, Action action)
+        {
+            Name = name;
+            Action = action;
         }
     }
 }
--- LibUR/GameStates/IGameStateObserver.cs
+++ LibUR/GameStates/IGameStateObserver.cs
@@ -4,8 +4,9 @@ namespace LibUR.GameStates
 {
     public interface IGameStateObserver
     {
-        void Subscribe(GameState state, Action action);
-        void Unsubscribe(GameState state);
+        void Subscribe(GameState state, Action action, string subscriberName);
+        void Unsubscribe(GameState state, string subscriberName);
         void Fire(GameState state);
+        void Clear();
     }
 }
