62ce5063f31cd8dd4793b8d72bf215082e060b2e
raco <cekcar@gmail.com>
2025-05-02 01:33:00 +0200

    GameStateObserver Updates

--- LibUR/GameStates/GameStateObserver.cs
+++ LibUR/GameStates/GameStateObserver.cs
@@ -6,38 +6,50 @@ namespace LibUR.GameStates
 {
     public class GameStateObserver : IGameStateObserver
     {
-        private readonly Dictionary<GameState, List<Subscriber>> _states;
+        private readonly Dictionary<GameState, List<SubscriberData>> _subscribers;
+        public Dictionary<GameState, List<SubscriberData>> Subscribers => _subscribers;
 
         public GameStateObserver()
         {
-            _states = new Dictionary<GameState, List<Subscriber>>();
+            _subscribers = new Dictionary<GameState, List<SubscriberData>>();
         }
 
         public void Subscribe(GameState state, Action action, string subscriberName)
         {
-            if (_states.ContainsKey(state))
+            if (_subscribers.ContainsKey(state))
             {
-                _states[state].Add(new Subscriber(subscriberName, action));
+                if (_subscribers[state].Any(x => x.Name == subscriberName))
+                {
+                    UnityEngine.Debug.LogWarning($"{subscriberName} already subscribed to {state}");
+                    return;
+                }
+
+                _subscribers[state].Add(new SubscriberData(subscriberName, action));
                 return;
             }
 
-            if (_states[state].Any(x => x.Name == subscriberName))
-                UnityEngine.Debug.LogWarning($"{subscriberName} already subscribed to {state}");
+            
             else
-                _states.Add(state, new List<Subscriber> { new Subscriber(subscriberName, action) });
+                _subscribers.Add(state, new List<SubscriberData> { new SubscriberData(subscriberName, action) });
         }
 
         public void Unsubscribe(GameState state, string subscriberName)
         {
-            if (!_states.ContainsKey(state))
+            if (!_subscribers.ContainsKey(state))
                 return;
 
-            _states[state].RemoveAll(x => x.Name == subscriberName);
+            _subscribers[state].RemoveAll(x => x.Name.Equals(subscriberName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public void Unsubscribe(string subscriberName)
+        {
+            foreach (var state in _subscribers)
+                state.Value.RemoveAll(x => x.Name.Equals(subscriberName, StringComparison.OrdinalIgnoreCase));
         }
 
         public void Fire(GameState state)
         {
-            foreach (var st in _states)
+            foreach (var st in _subscribers)
             {
                 if (!st.Key.Equals(state))
                     continue;
@@ -49,19 +61,7 @@ namespace LibUR.GameStates
 
         public void Clear()
         {
-            _states.Clear();
-        }
-    }
-
-    internal struct Subscriber
-    {
-        internal string Name;
-        internal Action Action;
-
-        public Subscriber(string name, Action action)
-        {
-            Name = name;
-            Action = action;
+            _subscribers.Clear();
         }
     }
 }
---
+++ LibUR/GameStates/SubscriberData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LibUR.GameStates
+{
+    public struct SubscriberData
+    {
+        internal string Name;
+        internal Action Action;
+
+        public SubscriberData(string name, Action action)
+        {
+            Name = name;
+            Action = action;
+        }
+    }
+}
