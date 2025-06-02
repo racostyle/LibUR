52d23595734f48b43cb8e6bdc44ef1c12efddb8e
raco <cekcar@gmail.com>
2025-06-02 19:32:24 +0200

    Delegate Wrappers update

--- LibUR/Delegates/WrappedFuncClasses.cs
+++ LibUR/Delegates/WrappedFuncClasses.cs
@@ -2,102 +2,99 @@
 
 namespace LibUR.Delegates
 {
-    public class WrappedFuncClasses
+    public class WrappedFunc<TX>
     {
-        public class WrappedFunc<TX>
+        private Func<TX> _func;
+
+        public void Register(Func<TX> func)
         {
-            private Func<TX> _func;
-
-            public void Register(Func<TX> func)
-            {
-                if (_func == null)
-                    _func = func;
-            }
-
-            public TX Invoke()
-            {
-                return _func.Invoke();
-            }
+            if (_func == null)
+                _func = func;
         }
 
-        public class WrappedFunc<T1, TX>
+        public TX Invoke()
         {
-            private Func<T1, TX> _func;
+            return _func.Invoke();
+        }
+    }
 
-            public void Register(Func<T1, TX> func)
-            {
-                if (_func == null)
-                    _func = func;
-            }
+    public class WrappedFunc<T1, TX>
+    {
+        private Func<T1, TX> _func;
 
-            public TX Invoke(T1 arg1)
-            {
-                return _func.Invoke(arg1);
-            }
+        public void Register(Func<T1, TX> func)
+        {
+            if (_func == null)
+                _func = func;
         }
 
-        public class WrappedFunc<T1, T2, TX>
+        public TX Invoke(T1 arg1)
         {
-            private Func<T1, T2, TX> _func;
+            return _func.Invoke(arg1);
+        }
+    }
 
-            public void Register(Func<T1, T2, TX> func)
-            {
-                if (_func == null)
-                    _func = func;
-            }
+    public class WrappedFunc<T1, T2, TX>
+    {
+        private Func<T1, T2, TX> _func;
 
-            public TX Invoke(T1 arg1, T2 arg2)
-            {
-                return _func.Invoke(arg1, arg2);
-            }
+        public void Register(Func<T1, T2, TX> func)
+        {
+            if (_func == null)
+                _func = func;
         }
 
-        public class WrappedFunc<T1, T2, T3, TX>
+        public TX Invoke(T1 arg1, T2 arg2)
         {
-            private Func<T1, T2, T3, TX> _func;
+            return _func.Invoke(arg1, arg2);
+        }
+    }
 
-            public void Register(Func<T1, T2, T3, TX> func)
-            {
-                if (_func == null)
-                    _func = func;
-            }
+    public class WrappedFunc<T1, T2, T3, TX>
+    {
+        private Func<T1, T2, T3, TX> _func;
 
-            public TX Invoke(T1 arg1, T2 arg2, T3 arg3)
-            {
-                return _func.Invoke(arg1, arg2, arg3);
-            }
+        public void Register(Func<T1, T2, T3, TX> func)
+        {
+            if (_func == null)
+                _func = func;
         }
 
-        public class WrappedFunc<T1, T2, T3, T4, TX>
+        public TX Invoke(T1 arg1, T2 arg2, T3 arg3)
         {
-            private Func<T1, T2, T3, T4, TX> _func;
+            return _func.Invoke(arg1, arg2, arg3);
+        }
+    }
 
-            public void Register(Func<T1, T2, T3, T4, TX> func)
-            {
-                if (_func == null)
-                    _func = func;
-            }
+    public class WrappedFunc<T1, T2, T3, T4, TX>
+    {
+        private Func<T1, T2, T3, T4, TX> _func;
 
-            public TX Invoke(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
-            {
-                return _func.Invoke(arg1, arg2, arg3, arg4);
-            }
+        public void Register(Func<T1, T2, T3, T4, TX> func)
+        {
+            if (_func == null)
+                _func = func;
         }
 
-        public class WrappedFunc<T1, T2, T3, T4, T5, TX>
+        public TX Invoke(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
         {
-            private Func<T1, T2, T3, T4, T5, TX> _func;
+            return _func.Invoke(arg1, arg2, arg3, arg4);
+        }
+    }
 
-            public void Register(Func<T1, T2, T3, T4, T5, TX> func)
-            {
-                if (_func == null)
-                    _func = func;
-            }
+    public class WrappedFunc<T1, T2, T3, T4, T5, TX>
+    {
+        private Func<T1, T2, T3, T4, T5, TX> _func;
 
-            public TX Invoke(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
-            {
-                return _func.Invoke(arg1, arg2, arg3, arg4, arg5);
-            }
+        public void Register(Func<T1, T2, T3, T4, T5, TX> func)
+        {
+            if (_func == null)
+                _func = func;
+        }
+
+        public TX Invoke(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+        {
+            return _func.Invoke(arg1, arg2, arg3, arg4, arg5);
         }
     }
 }
